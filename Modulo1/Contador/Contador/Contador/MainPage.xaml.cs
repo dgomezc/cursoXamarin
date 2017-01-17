af834ed600f11319f4ad2f55ab56081cf90925a2
david <davidgomezcano@outlook.com>
2017-01-17 22:34:02 +0100

    Primer reto del curso xamarin

---
+++ Modulo1/Contador/Contador/Contador/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Contador
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            boton.Clicked += (s, e) =>  label.Text= string.Format("Botón pulsado {0} veces", ++count);
+        }        
+    }
+}
